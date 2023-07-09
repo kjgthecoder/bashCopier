032bd65b001862a7d422eab0dbd0885d736d6b76
kjgthecoder <98246960+kjgthecoder@users.noreply.github.com>
2023-07-08 19:36:12 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,59 @@
+# bashcopier
+
+## Introduction
+
+This software initiative was born from my experiences in Professor Corkan's Linux/Unix course at the University of Central Arkansas. Throughout the semester, we frequently engaged in assignments that involved copying sample scripts from PDF documents into our terminal environment. These tasks served to deepen our understanding of the concepts elucidated during our lectures.
+
+However, one significant challenge was that pasting from a PDF into vim often led to the loss of original formatting. In addition, the process retained all line numbers, which proved to be a nuisance. Recognizing this issue, I sought to develop a program that would bridge this gap, streamlining and automating the process.
+
+The envisioned program functions as an intermediary - users can copy content from a PDF into the application, which then presents the correctly formatted code. This code is then automatically copied to the clipboard, ready to be pasted directly into vim.
+
+## Enhancements
+
+During the development process, I identified areas for enhancement within the formatting function. To make the tool more user-friendly, I decided to incorporate interactive elements. This led to the addition of buttons that can adjust the indentation of each line. Any modifications made are instantly reflected in the clipboard contents, ensuring that users always have access to the most recent version of their code.
+
+## Drawbacks
+
+If it is a long script (longer than 20 lines) the GUI's length may exceed the length of a horizontal monitor. 
+
+## Dependencies
+
+The following libraries are needed if not already installed: 
+
+- tkinter
+- pyperclip
+- re
+
+## Sample Scripts
+
+Below are some example scripts to try:
+
+**Example 1** - Copying Code from VIM (white space precedes each number line)
+```bash
+  1 #!/bin/bash
+  2
+  3 # Check number of arguments
+  4 if [ $# -ne 1 ]; then
+  5     echo "Usage: ./count_files.sh directory"
+  6     exit 1
+  7 fi
+  8
+  9 # Check if argument is directory
+ 10 if [ ! -d "$1" ]; then
+ 11     echo "Error: First parameter must be a directory (dir)."
+ 12     exit 1
+ 13 fi
+ ```
+
+**Example 2** - Copying code from a PDF
+```bash
+1 #!/bin/bash
+2 read -p "word1:" word1
+3 read -p "word2:" word2
+4 if test "$word1" = "$word2"
+5 then
+6 echo "Match"
+7 fi
+```
+
+
---
+++ Root.py
@@ -0,0 +1,102 @@
+import EventHandler 
+import tkinter as tk 
+import re 
+import pyperclip
+
+class Root:
+    def __init__(self, event_handler): 
+        # Root
+        self.root = tk.Tk() 
+        self.root.geometry("800x400")
+        self.root.title("EventHandlerPractice")
+        self.root.resizable(False, False)
+        self.root.config(bg="#F7DCED")
+
+        self.code_input = "" # bash_script 
+        self.code_lines_to_format = []
+
+        self.event_handler = event_handler
+        self.event_handler.subscribe("PASTE_EVENT", self.handle_paste_event) # Subscribing to PASTE_EVENT
+        self.event_handler.subscribe("CLEAR_EVENT", self.handle_clear_event) # Subscribing to CLEAR_EVENT
+
+    def scrapeDaNumbas(self):
+        """
+        Strips text from line numbers & splits script into an array. 
+        """
+        lines = self.code_input.splitlines()
+        for i in range(len(lines)):
+            # Checking to see if there are numbers lines in the beginning using regex
+            # Also accounts for space chars preceding the line number if actually copying from 
+            # vim editor.  
+            num_regex = re.match(r"^\s*(\d+\s*)?", lines[i])
+            if (num_regex is not None): 
+                num_str = num_regex.group()
+                # Splcing the string based on the length of the regex match 
+                lines[i] = lines[i][len(num_str):]
+            else: 
+                lines[i] = lines[i].lstrip() 
+        for line in self.code_lines_to_format: 
+            print(line)
+        self.code_lines_to_format = lines
+        copied_text = pyperclip.copy(("\n").join(lines))
+
+    def try_parse_int(self, num_str): 
+        """
+        Returns true or false if string passed can be a number or not
+        """
+        if num_str == None: 
+            return False 
+        try: 
+            number = int(num_str) 
+            return True 
+        except ValueError: 
+            return False 
+
+    def formatBashScript(self, indent_size=4):
+        """
+        Adds indentation to final output. 
+        """
+        indented_code = ""
+        indent_level = 0 
+
+        for line in self.code_lines_to_format:
+            stripped_line = line.strip()
+
+            if stripped_line.startswith(("if", "elif", "else", "for", "while", "until", "case")):
+                indented_line = " " * (indent_size * indent_level) + line
+                indent_level += 1
+            elif stripped_line.startswith(("done", "fi")):
+                indent_level -= 1
+                indented_line = " " * (indent_size * indent_level) + line
+            else:
+                indented_line = " " * (indent_size * indent_level) + line
+
+            indented_code += indented_line + "\n"
+        
+        return indented_code.splitlines()
+
+    def handle_paste_event(self, event, data): 
+        """
+        Receieves "PASTE_EVENT" from InputFrame, which then formats the code a bit 
+        and puts the output of the now formatted code into the OutputFrame. 
+        It then sends a "PASTE_EVENT_SCRAPE" event to OutputFrame
+        """
+        print("\nRoot: handle_paste_event")
+        # Formatting text 
+        self.code_input = data
+        self.scrapeDaNumbas() 
+        final_out = self.formatBashScript() 
+        # Sending PASTE_EVENT_SCRAPE to Outputframe along w/ formatted code (code_input_parsed)
+        self.event_handler.publish("PASTE_EVENT_SCRAPE", final_out)
+
+    def handle_clear_event(self, event, data): 
+        """
+        Throws a signal to  the output frame to begin chain of events for when 
+        the clear button is pressed. 
+        """
+        print("\nRoot: handle_clear_event") 
+        
+
+
+
+
